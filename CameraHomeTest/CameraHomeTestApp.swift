df836ec5ec1ea4b965c17365b9578f9a9e6cdda9
Maks Barbaruk <maksbarbaruk@gmail.com>
2026-08-17 21:14:18 +0300

    Initial Commit

---
+++ CameraHomeTest/CameraHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraHomeTestApp.swift
+//  CameraHomeTest
+//
+//  Created by Maks Barbaruk on 17.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct CameraHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
